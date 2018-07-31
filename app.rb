e84e23bacdec55feba2e18f128360a28763f6fcc
Kayvan Vakili <kvakili813@gmail.com>
2018-07-31 22:05:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
-require_relative 'config/environment'
+require './environment'
 
-class App < Sinatra::Base
+module FormsLab
+  class App < Sinatra::Base
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
+
+  end
 end
